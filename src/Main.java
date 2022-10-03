15871ec40d6669d5c1ad9ddef980d8faefed7069
anna-ermakova <korovanna8@mail.ru>
2022-10-03 17:41:15 +0300

    Задание 2 исправила

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,19 @@ public class Main {
         }
         //task2
         System.out.println("Задание 2");
-        int clientOSis = 0;// IOS-0, Android-1
-        int year = 2019; // год выпуска
-        if (clientOSis == 0 && year >= 2015) {
+        int clientOSis = 1;// IOS-0, Android-1
+        int clientDeviceYear = 2015; // год выпуска
+        if (clientOSis == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOSis == 0 && year < 2015) {
+        } else if (clientOSis == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOSis == 1 && year < 2015) {
+        }if (clientOSis == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else {
+        } else if (clientOSis == 1 && clientDeviceYear >= 2015){
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+        //task3
+        System.out.println("Задание 3");
 
     }
     }
