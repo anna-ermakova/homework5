5f3d6fd9cac751373d832fed8719ac37b72725d6
anna-ermakova <korovanna8@mail.ru>
2022-10-04 12:47:04 +0300

    Задание 5 исправила

--- src/Main.java
+++ src/Main.java
@@ -48,43 +48,27 @@ public class Main {
 
         //task5
         System.out.println("Задание 5");
-        int monthNumber=66;
+        int monthNumber=6;
         switch (monthNumber){
+            case 12:
             case 1:
-                System.out.println("Зима");
-                break;
             case 2:
                 System.out.println("Зима");
                 break;
             case 3:
-                System.out.println("Весна");
-                break;
             case 4:
-                System.out.println("Весна");
-                break;
             case 5:
                 System.out.println("Весна");
                 break;
             case 6:
-                System.out.println("Лето");
-                break;
             case 7:
-                System.out.println("Лето");
-                break;
             case 8:
                 System.out.println("Лето");
                 break;
             case 9:
-                System.out.println("Осень");
-                break;
             case 10:
-                System.out.println("Осень");
-                break;
             case 11:
                 System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
                 break;
             default:
                 System.out.println("Такого месяца не существует");
